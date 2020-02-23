62bae79a58af3cb3b058d1230a959e5e8a1b833f
alex1431999 <ah01242@surrey.ac.uk>
2020-02-23 11:24:31 +0000

    Implement celery support

--- controller.py
+++ controller.py
@@ -2,9 +2,11 @@
 Module to hanlde different scripts that the crawler can execute
 """
 
-from common.mongo.data_types.keyword import Keyword
+from common.mongo.controller import MongoController
+from common.celery import queues
 
 from crawler import NewsCrawler
+from tasks import app
 
 class Controller():
     """
@@ -15,3 +17,47 @@ class Controller():
         Initialise the crawler
         """
         self.crawler = NewsCrawler()
+        self.mongo_controller = MongoController()
+
+    def __save_news_result(self, news_result):
+        """
+        Save a news article to the MongoDb
+
+        :param dict news_result: The result received packaged up
+        """
+        # Remove the trailing Z from timestamp
+        news_result['timestamp'] = news_result['timestamp'][:-1]
+
+        crawl = self.mongo_controller.add_crawl_news(
+            news_result['keyword_id'],
+            news_result['author'],
+            news_result['title'],
+            news_result['text'],
+            news_result['timestamp'],
+            return_object=True,
+            cast=True
+        )
+
+        app.send_task('process-crawl', kwargs={ 'crawl_dict': crawl.to_json() }, queue=queues['processor'])
+
+        return crawl
+
+    def __save_news_results(self, news_results):
+        """
+        Save all news articles
+
+        :param list<dict> news_results: The to be saved news results
+        """
+        crawls = [self.__save_news_result(result) for result in news_results]
+        return crawls
+
+    def run_single_keyword(self, keyword_string, language):
+        """
+        Crawl a single keyword
+
+        :param str keyword_string: Target keyword string
+        :param str language: Target language
+        """
+        keyword = self.mongo_controller.get_keyword(keyword_string, language, cast=True)
+        news_results = self.crawler.search(keyword)
+        return self.__save_news_results(news_results)
---
+++ tasks.py
@@ -0,0 +1,30 @@
+"""
+This module defines all the Celery tasks which this crawler can execute.
+It also runs all the setup required for celery to function.
+"""
+import os
+
+from common.utils.logging import DEFAULT_LOGGER, LogTypes
+from common.celery import queues
+from celery import Celery
+
+app = Celery('tasks',
+    broker = os.environ['BROKER_URL']
+)
+
+from controller import Controller
+
+controller = Controller()
+
+@app.task(name='crawl-news-keyword', queue=queues['news'])
+def crawl_twitter_keyword(keyword_string, language):
+    """
+    Crawl a single keyword Task
+
+    :param str keyword_string: The target keyword string
+    :param str language: The target language
+    """
+    DEFAULT_LOGGER.log('Received news keyword crawl request for {} ({})'.format(keyword_string, language), log_type=LogTypes.INFO.value)
+    result = controller.run_single_keyword(keyword_string, language)
+
+    return True if result else False
